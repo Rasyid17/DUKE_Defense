fe800774611e1e180b70bb2db694e1d619d741c7
Rasyid17 <rex.oxide@gmail.com>
2016-09-09 10:12:40 +0800

    1st update

---
+++ Assets/Script/LaserGunShotBehaviour.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaserGunShotBehaviour : MonoBehaviour {
+
+    public float BulletSpeed;
+    	
+	// Update is called once per frame
+	void Update () {
+		transform.position += transform.forward * Time.deltaTime * BulletSpeed;
+	}
+}
